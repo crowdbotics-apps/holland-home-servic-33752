a294ed1f26e82330d4b1c930dc37a3e8c0f50326
Crowdbotics <team@crowdbotics.com>
2022-02-21 05:46:49 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/holland_home_servic_33752/MainActivity.java
@@ -0,0 +1,15 @@
+package com.holland_home_servic_33752;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "holland_home_servic_33752";
+  }
+}
